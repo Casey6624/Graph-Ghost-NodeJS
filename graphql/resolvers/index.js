f4d2fc01480d0a63e2ff60533d3d01d26ef9f2b8
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-09-23 14:09:39 +0100

    Added basic user creation and validation to check if user already exists

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -8,5 +8,25 @@ module.exports = GraphQLResolvers = {
       { _id: "123", email: "casey@test.com" },
       { _id: "456", email: "test@test.com" }
     ];
+  },
+  createUser: async ({ userInput }) => {
+    const { email } = userInput;
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      throw new Error("User already exists!");
+    }
+
+    const user = new User({
+      email: email
+    });
+    return user
+      .save()
+      .then(() => {
+        return user;
+      })
+      .catch(err => {
+        throw err;
+      });
   }
 };

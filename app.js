f66d228b379f517a934332f436368c11e1e46b0f
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-10-29 19:55:09 +0000

    Tried to fix CORS error by using the cors package, however this wasn't successful

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const graphqlHttp = require("express-graphql");
+const cors = require("cors");
 const app = express();
-app.use(bodyParser.json());
 // Constants
 const NODE_PORT = 4500;
 const MONGO_PORT = 4501;
@@ -16,9 +16,11 @@ const graphqlResolvers = require("./graphql/resolvers/index");
 const graphqlSchema = require("./graphql/schema/index");
 // Models
 const Code = require("./models/Code");
+//app.use(bodyParser.json());
+app.use(cors());
 
 // Middleware to combat CORS errors
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -26,7 +28,7 @@ app.use((req, res, next) => {
     return res.sendStatus(200);
   }
   next();
-});
+}); */
 
 // use defined schemas and resolvers
 app.use(

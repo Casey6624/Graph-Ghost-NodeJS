7cb5238300e54e10fd34456a2c0183d91c991f7c
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2020-04-20 14:46:32 +0100

    Add jest into scripts and create jest.config.js

---
+++ graphql/resolvers/resolvers.test.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+beforeAll(async () => {
+    mongoose
+.connect(
+    const MONGO_PORT = 4501;
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphghost-966hz.azure.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+})
